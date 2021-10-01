fdb36d4733eb43f830a82b9e63e038346de370c6
RBioZ <jonatha_rihan@hotmail.com>
2021-10-01 19:28:15 -0300

    chore: Base and Icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import iphone from './assets/iphone.png';
 // import { Container } from './styles';
+import logo from './assets/logo.png';
+
 
 const src: React.FC = () => {
-  return <div />;
+  return (
+    <div>
+      <img src={logo} />
+      <img src={iphone} />
+    </div>
+  )
 }
 
 export default src;
